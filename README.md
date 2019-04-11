45584e4d286d887f8e38f75463e8768138cf7b2a
mallowlabs <mallowlabs@gmail.com>
2019-04-11 11:01:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# redash-proxy-growthforecast
+
+A proxy lambda function to convert GrowthForecast JSON to Redash datasource.
+
+
+## Requirements
+
+* AWS CLI
+* SAM CLI
+
+## Deploy
+
+```bash
+$ aws s3 mb s3://your-sandbox --region ap-northeast-1
+```
+
+```bash
+$ cd redash-proxy-growthforecast
+$ bundle
+```
+
+```bash
+$ sam package \
+    --template-file template.yaml \
+    --output-template-file serverless-output.yaml \
+    --s3-bucket your-sandbox
+```
+
+```bash
+$ sam deploy \
+    --template-file serverless-output.yaml \
+    --stack-name your-redash-proxy-growthforecast \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides \
+      GrowthForecastRoot=your-growth-forecast-root \
+      GrowthForecastUsername=your-growth-forecast-username \
+      GrowthForecastPassword=your-growth-forecast-password
+```
+
+Confirm your endpoint url.
+
+```bash
+$ aws cloudformation describe-stacks --stack-name your-redash-proxy-growthforecast --region ap-northeast-1
+```
+
+### Redash Side
+
+Add `URL Data source`.
+
+`Name`: Input friendly name.
+`URL base path`: Input your endpoint url. ex: `https://your.execute-api.ap-northeast-1.amazonaws.com/Prod/proxy/`
+`HTTP Basic Auth Username`: Input your GrwothForecast username (Basic Auth).
+`HTTP Basic Auth Password`: Input your GrwothForecast password (Basic Auth).
+
+Adde `Query`.
+
+Input `{service_name}/{section_name}/{graph_name}`.
+You can use query parameter. ex: `?t=y`.
---
+++ redash-proxy-growthforecast/app.rb
@@ -0,0 +1,85 @@
+require 'httparty'
+require 'active_support'
+require 'active_support/core_ext'
+require 'json'
+require 'base64'
+
+def lambda_handler(event:, context:)
+  growth_forecast_root = ENV['GROWTH_FORECAST_ROOT']
+  growth_forecast_username = ENV['GROWTH_FORECAST_USERNAME']
+  growth_forecast_password = ENV['GROWTH_FORECAST_PASSWORD']
+
+  # basic auth
+  unless auth?(event, growth_forecast_username, growth_forecast_password)
+    return {
+      headers: {'WWW-Authenticate': "Basic realm=\"Secret Zone\""},
+      statusCode: 401,
+      body: {ok: false}.to_json
+    }
+  end
+
+  begin
+    service_name = event['pathParameters']['service_name']
+    section_name = event['pathParameters']['section_name']
+    graph_name = event['pathParameters']['graph_name']
+    query = event['queryStringParameters'] || {}
+    url = "#{growth_forecast_root}/xport/#{service_name}/#{section_name}/#{graph_name}?#{query.to_query}"
+
+    response = HTTParty.get(url, {basic_auth: {username: growth_forecast_username, password: growth_forecast_password}})
+
+    json = JSON.parse(response.body)
+    body = convert(json)
+    {
+      statusCode: response.code,
+      body: body.to_json
+    }
+  rescue HTTParty::Error => error
+    puts error.inspect
+    raise error
+  end
+end
+
+def auth?(event, growth_forecast_username, growth_forecast_password)
+  authorization =  event['headers']['Authorization'] || ''
+  authorization.gsub(/Basic\s+/, '').strip ==
+    Base64.encode64("#{growth_forecast_username}:#{growth_forecast_password}").strip
+end
+
+def convert(json)
+  column_names = json['column_names']
+  rows = json['rows']
+  start_timestamp = json['start_timestamp']
+  step = json['step']
+
+  # columns
+  redash_columns = [
+    {
+      name: "date",
+      type: "date",
+      friendly_name: "date"
+    }
+  ].concat(column_names.map { |c|
+    {
+      name: c,
+      type: "integer",
+      friendly_name: c
+    }
+  })
+
+  # rows
+  redash_rows = rows.map { |cells|
+    hash = {}
+    hash['date'] = Time.at(start_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    cells.each_with_index { |c, i|
+      hash[column_names[i]] = c
+    }
+    start_timestamp += step
+    hash
+  }
+
+  # body
+  {
+    columns: redash_columns,
+    rows: redash_rows
+  }
+end
